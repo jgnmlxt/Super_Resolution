f0808fd7516277d0fd60febc221f67d60861d176
jgnmlxt <jgnmlxt@users.noreply.github.com>
2025-03-04 19:01:31 -0500

    created_shited_images done

--- compress.m
+++ compress.m
@@ -1,27 +1,45 @@
-function compressed_image=compress(image,compressed_factor)
-    [rows,cols,~]=size(image);
+function compressed_image_series=compress(image_series,compressing_factor)
     
-    if ~((mod(rows,compressed_factor)==0)&&(mod(cols,compressed_factor)==0))
-        error("The size of the image cannot be compressed with such factor.")
+    if class(image_series)=="double"
+
+        image_series={image_series};
+
     end
 
-    compressed_image=zeros(rows/compressed_factor,cols/compressed_factor);
-    temp_1=zeros(rows,cols/compressed_factor);
+    compressed_image_series=cell(1,length(image_series));
 
-    for i=1:cols/compressed_factor
-        temp=zeros(rows,1);
-        for j=1:compressed_factor
-            temp=temp+image(:,(i-1)*compressed_factor+j);
+    for counter=1:length(image_series)
+
+        image=image_series{counter};
+
+        [rows,cols,~]=size(image);
+        
+        if ~((mod(rows,compressing_factor)==0)&&(mod(cols,compressing_factor)==0))
+            error("The size of the image cannot be compressed with such factor.")
         end
-        temp_1(:,i)=temp;
-    end
 
-    for i=1:rows/compressed_factor
-        temp=zeros(1,cols/compressed_factor);
-        for j=1:compressed_factor
-            temp=temp+temp_1((i-1)*compressed_factor+j,:);
+        dbclear if error
+    
+        compressed_image=zeros(rows/compressing_factor,cols/compressing_factor);
+        temp_1=zeros(rows,cols/compressing_factor);
+    
+        for i=1:cols/compressing_factor
+            temp=zeros(rows,1);
+            for j=1:compressing_factor
+                temp=temp+image(:,(i-1)*compressing_factor+j);
+            end
+            temp_1(:,i)=temp;
         end
-        compressed_image(i,:)=temp;
+    
+        for i=1:rows/compressing_factor
+            temp=zeros(1,cols/compressing_factor);
+            for j=1:compressing_factor
+                temp=temp+temp_1((i-1)*compressing_factor+j,:);
+            end
+            compressed_image(i,:)=temp;
+        end
+        compressed_image=compressed_image/(compressing_factor^2);
+        compressed_image_series{counter}=compressed_image;
+
     end
-    compressed_image=compressed_image/(compressed_factor^2);
 end
--- create_shifted_images.m
+++ create_shifted_images.m
@@ -1,13 +1,12 @@
-function create_shifted_images(GT,compressing_factor)
-    image_series={};
-    
+function image_series=create_shifted_images(GT,compressing_factor)
+    image_series=cell(1,compressing_factor^2);
     
     for i=1:compressing_factor
         for j=1:compressing_factor
             shifted_GT=zeros(size(GT));
-            shifted_GT(:,1:end-1)=GT(:,2:end);
+            shifted_GT(1:end-(i-1),1:end-(j-1))=GT(i:end,j:end);
+            image_series{(i-1)*compressing_factor+j}=shifted_GT;
         end
     end
-    
     
 end
