90bc4b747dfb3bb252c6f9448c921f8269ef2ed0
jgnmlxt <jgnmlxt@users.noreply.github.com>
2025-04-06 20:22:28 -0400

    adding lego-like recon

---
+++ 3D_4/get_inputs.m
@@ -0,0 +1,17 @@
+function [xy_z,yz_x,xz_y,two_xy]=get_inputs(GT)
+    [x,y,z]=size(GT);
+    if (~x==y)&&(~y==z)
+        error("Dimension of GT is wrong; it should have same number of pixels in all dimension.");
+    end
+
+    if (~mod(x,4)==0)
+        error("GT should have a size of 4n x 4n x 4n");
+    end
+
+    temp=GT(1:2:end,:,:)+GT(2:2:end,:,:);
+    two_xy=temp(:,1:2:end,:)+temp(:,2:2:end,:);
+
+    xy_z=GT(:,:,1:4:end)+GT(:,:,2:4:end)+GT(:,:,3:4:end)+GT(:,:,4:4:end);
+    yz_x=GT(1:4:end,:,:)+GT(2:4:end,:,:)+GT(3:4:end,:,:)+GT(4:4:end,:,:);
+    xz_y=GT(:,1:4:end,:)+GT(:,2:4:end,:)+GT(:,3:4:end,:)+GT(:,4:4:end,:);
+end
---
+++ 3D_4/main.m
@@ -0,0 +1,4 @@
+GT=phantom3d(36);
+[xy_z,yz_x,xz_y,two_xy]=get_inputs(GT);
+
+%% 
